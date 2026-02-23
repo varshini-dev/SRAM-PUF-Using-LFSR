e4167233b8343ee64791ad782042eca6775ca28c
Varshini Ganesan <varshini.g.5126@gmail.com>
2026-02-24 01:53:58 +0530

    Update sram_puf.v

--- sram_puf.v
+++ sram_puf.v
@@ -1,54 +1,15 @@
 `timescale 1ns / 1ps
 
-module testbench;
-    parameter WIDTH = 128;
+module sram_puf #(parameter WIDTH = 128)(
+    input clk,
+    input enable,
+    output reg [WIDTH-1:0] sram_out
+);
 
-    reg clk;
-    reg rst;
-    reg enable;
-
-    wire [WIDTH-1:0] puf_response;
-
-    // Instantiate DUT
-    top_mod #(WIDTH) DUT (
-        .clk(clk),
-        .rst(rst),
-        .enable(enable),
-        .puf_response(puf_response)
-    );
-
-    // Clock generation (10ns period)
-    always #5 clk = ~clk;
-
-    initial begin
-        // Initialize
-        clk = 0;
-        rst = 1;
-        enable = 0;
-
-        // Hold reset
-        #20;
-        rst = 0;
-
-        // Trigger SRAM capture
-        #10;
-        enable = 1;
-
-        #10;
-        enable = 0;   // Load only once
-
-        // Let LFSR run for 50 cycles
-        #500;
-
-        $display("Final Extracted PUF Response:");
-        $display("%b", puf_response);
-
-        $finish;
-    end
-
-    // Monitor signal changes
-    initial begin
-        $monitor("Time=%0t | Response=%h", $time, puf_response);
+    always @(posedge clk) begin
+        if (enable) begin
+            sram_out <= {$urandom, $urandom, $urandom, $urandom};
+        end
     end
 
 endmodule
